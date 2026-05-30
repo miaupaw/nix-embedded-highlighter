4a7853ab154e9a62dde0fe2504ecd51ba153e5c7
oom <webmaster@spicebrains.com>
2026-05-30 13:06:55 +0300

    commit
    feat: add 7 new language grammars (ruby, toml, sql, lua, js, ini, perl), bump to 0.1.0

--- flake.nix
+++ flake.nix
@@ -16,27 +16,26 @@
           buildInputs = with pkgs; [
             nodejs_22
             vsce
-            ovsx
           ];
 
           shellHook = '' # bash
             echo "VS Code Extension Dev Environment"
             echo "Run 'vsce package' to build the extension."
-            echo "Run 'ovsx publish' to release to Open VSX."
+            echo "Run 'npm exec ovsx publish' to release to Open VSX."
           '';
         };
 
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "nix-embedded-highlighter";
-          version = "0.0.1";
+          version = "0.1.0";
           src = ./.;
-          
+
           nativeBuildInputs = [ pkgs.nodejs_22 pkgs.vsce ];
 
           buildPhase = '' # bash
             # vsce needs a package.json to work
             # We skip 'npm install' because we have no dependencies yet
-            vsce package --out nix-embedded-highlighter-0.0.1.vsix
+            vsce package --out nix-embedded-highlighter-0.1.0.vsix
           '';
           installPhase = '' # bash
             mkdir -p $out/share/vscode/extensions
